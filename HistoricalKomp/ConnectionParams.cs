43f203838ec20bdb93708caa8158499bb635b79d
Tatjana Kosić <kosic.pr33.2020@uns.ac.rs>
2023-01-03 17:09:24 +0100

    Izmene

---
+++ HistoricalKomp/ConnectionParams.cs
@@ -0,0 +1,35 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalKomp
+{
+    public class ConnectionParams : IDisposable
+    {
+        private static IDbConnection instance = null;
+        public static readonly string DATA_SOURCE = "//localhost:1521/xe";
+        public static readonly string USER_ID = "tatj";
+        public static readonly string PASSWORD = "ftn";
+
+        public static IDbConnection GetConnection()
+        {
+            OracleConnectionStringBuilder ocsb = new OracleConnectionStringBuilder();
+            ocsb.UserID = USER_ID;
+            ocsb.DataSource = DATA_SOURCE;
+            ocsb.Password = PASSWORD;
+
+            instance = new OracleConnection(ocsb.ConnectionString);
+
+            return instance;
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HistoricalKomp/Historical.cs
+++ HistoricalKomp/Historical.cs
@@ -8,5 +8,6 @@ namespace HistoricalKomp
 {
     public class Historical
     {
+
     }
 }
