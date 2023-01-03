1aad81b5aba07b338fe5c8d271ee26576634b81f
Tatjana Kosić <kosic.pr33.2020@uns.ac.rs>
2023-01-03 17:57:23 +0100

    Promeneeeee

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HistoricalKomp;
 
 namespace CacheMemory
 {
     public class Program
     {
+       // IDbConnection conn = HistoricalKomp.ConnectionParams.GetConnection();
     }
 }
