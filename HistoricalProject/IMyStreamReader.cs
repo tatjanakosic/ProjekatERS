55ff9199eec521cd85048cfe0c8351118a6a3343
Tatjana Kosić <kosic.pr33.2020@uns.ac.rs>
2023-01-19 23:09:10 +0100

    Interfejs MyStreamReader

---
+++ HistoricalProject/IMyStreamReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalProject
+{
+    public interface IMyStreamReader
+    {
+        string ReadLine();
+        void Open(string fileName);
+        void Close();
+    }
+}
