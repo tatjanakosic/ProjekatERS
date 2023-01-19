96c7923ead93ce80f840b1d45544bfc5e647a7b9
Tatjana Kosić <kosic.pr33.2020@uns.ac.rs>
2023-01-19 23:08:32 +0100

    Interfejs Historical

---
+++ HistoricalProject/IHistorical.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalProject
+{
+    public interface IHistorical
+    {
+
+        bool SaveData(List<Data> data);
+        List<Data> GetDataById(int dataId);
+        List<Data> GetDataByAdresa(string adresa);
+
+        List<Data> GetDataByMesec(string mesec);
+        List<Data> GetAllData();
+    }
+}
