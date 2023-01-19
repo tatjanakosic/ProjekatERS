f62613854eb7cd6f2380a9258d7aa30e7cf3df9a
Tatjana Kosić <kosic.pr33.2020@uns.ac.rs>
2023-01-19 23:07:11 +0100

    Dodavanje MyStreamReader

---
+++ HistoricalProject/MyStreamReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalProject
+{
+    public class MyStreamReader : IMyStreamReader
+    {
+        public StreamReader Reader { get; set; }
+
+        public MyStreamReader() 
+        {
+        
+        }
+        public virtual void Close()
+        {
+            Reader.Close();
+        }
+
+        public virtual void Open(string fileName)
+        {
+            Reader = new StreamReader(fileName);
+        }
+
+        public virtual string ReadLine()
+        {
+            return Reader.ReadLine();
+        }
+    }
+}
