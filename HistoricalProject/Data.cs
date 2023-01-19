d9f24dcf9f606b5b2eba369668bfdd7729e1e6e3
Tatjana Kosić <kosic.pr33.2020@uns.ac.rs>
2023-01-19 23:04:38 +0100

    Dodavanje klase Data

---
+++ HistoricalProject/Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalProject
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public double Potrosnja { get; set; }
+        public string Adresa { get; set; }
+        public string Mesec { get; set; }
+
+        public Data(int id, double potrosnja, string adresa, string mesec)
+        {
+            Id = id;
+            Potrosnja = potrosnja;
+            Adresa = adresa;
+            Mesec = mesec;
+        }
+
+        public override string ToString()
+        {
+            return Id + "|" + Potrosnja + "|" + Adresa + "|" + Mesec;
+        }
+    }
+}
