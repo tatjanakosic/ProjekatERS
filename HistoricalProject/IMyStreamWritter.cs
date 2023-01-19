5c8b21971a47b1ea8881ccfaf6ac083550c4c645
Tatjana Kosić <kosic.pr33.2020@uns.ac.rs>
2023-01-19 23:09:54 +0100

    Interfejs MyStreamWriter

---
+++ HistoricalProject/IMyStreamWritter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalProject
+{
+    public interface IMyStreamWritter
+    {
+        void WriteLine(string line);
+        void Open(string fileName);
+        void Close();
+    }
+}
