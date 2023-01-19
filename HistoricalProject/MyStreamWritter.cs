68afeb2cdbedcbc6f1c2fa6610b8d2ff367f03ab
Tatjana Kosić <kosic.pr33.2020@uns.ac.rs>
2023-01-19 23:07:42 +0100

    Dodavanje MyStreamWriter

---
+++ HistoricalProject/MyStreamWritter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalProject
+{
+    public class MyStreamWritter : IMyStreamWritter
+    {
+        public StreamWriter Writter { get; set; }
+
+        public MyStreamWritter() 
+        {
+            
+        }
+
+
+        public virtual void WriteLine(string line)
+        {
+            Writter.WriteLine(line);
+        }
+
+        public virtual void Open(string fileName)
+        {
+            if (!File.Exists(fileName)) 
+            {
+                File.Create(fileName).Dispose();
+            }
+
+            Writter = new StreamWriter(fileName, true);
+        }
+
+        public virtual void Close()
+        {
+            Writter.Close();
+        }
+    }
+}
