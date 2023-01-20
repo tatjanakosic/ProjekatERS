01d6f757a649f0d5148a955f63006053e43bf09c
Sanja Jovisic <jovisic.pr25.2020@uns.ac.rs>
2023-01-20 17:48:09 +0100

    Dodavanje test klase za Writer

---
+++ CacheMemoryTest/WriterTest.cs
@@ -0,0 +1,32 @@
+﻿using DumpingBufferProject;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WriterProject;
+
+namespace CacheMemoryTest
+{
+    public class WriterTest
+    {
+
+        [Test]
+        public void WriterEmptyConstructorTest() 
+        {
+            Writer writer = new Writer();
+
+            Assert.IsNotNull(writer.DumpingBuffer);
+        }
+
+        [Test]
+        public void WriterConstructorwithParametersTest()
+        {
+            DumpingBuffer db = new DumpingBuffer();
+            Writer writer = new Writer(db);
+
+            Assert.IsNotNull(writer.DumpingBuffer);
+        }
+    }
+}
