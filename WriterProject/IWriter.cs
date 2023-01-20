57c1bbcd7eefea3ae1d59bd49b52ea547f1fa63d
Anja Sabojlovic <sabojlovic.pr27.2020@uns.ac.rs>
2023-01-20 16:40:19 +0100

    Dodavanje interfejsa writera

---
+++ WriterProject/IWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriterProject
+{
+    public interface IWriter
+    {
+
+        void SendData(int id, double potrosnja, string adresa, string mesec);
+    }
+}
