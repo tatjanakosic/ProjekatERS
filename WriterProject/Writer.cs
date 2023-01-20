0c0548f511847b6c6e80be5fa890d0e98757df12
Anja Sabojlovic <sabojlovic.pr27.2020@uns.ac.rs>
2023-01-20 16:38:55 +0100

    Dodavanje writer komponente

---
+++ WriterProject/Writer.cs
@@ -0,0 +1,30 @@
+﻿using DumpingBufferProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriterProject
+{
+    public class Writer : IWriter
+    {
+        public IDumpingBuffer DumpingBuffer { get; set; }
+
+
+        public Writer(IDumpingBuffer dumpingBuffer) 
+        {
+            DumpingBuffer = dumpingBuffer;
+        }
+
+        public Writer() 
+        {
+            DumpingBuffer = new DumpingBuffer();
+        }
+
+        public void SendData(int id, double potrosnja,string adresa, string mesec)
+        {
+            DumpingBuffer.SaveDataTemporary(id, potrosnja,adresa,mesec);
+        }
+    }
+}
