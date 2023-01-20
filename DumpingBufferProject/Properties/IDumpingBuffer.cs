4049412e4e44c410f20ff607e84eb98ebab5ed7d
Nevena Marjanovic <nevenamarjanovi@gmail.com>
2023-01-20 18:13:14 +0100

    Izmjena propertija

---
+++ DumpingBufferProject/Properties/IDumpingBuffer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumpingBufferProject
+{
+    public interface IDumpingBuffer
+    {
+        void SaveDataTemporary(int id, double potrosnja, string adresa, string mesec);
+
+        void SendToHistorical();
+
+        bool CheckState();
+    }
+}
