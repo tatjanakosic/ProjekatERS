04d4e4a789f6e7a9f0558cde1ab87501ff4a60d7
Anja Sabojlovic <sabojlovic.pr27.2020@uns.ac.rs>
2023-01-20 16:45:02 +0100

    Dodavanje interfejsa

---
+++ ReaderProject/IReader.cs
@@ -0,0 +1,22 @@
+﻿using HistoricalProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderProject
+{
+    public interface IReader
+    {
+        void WriteData(List<Data> data);
+
+        bool WriteAllData();
+
+        bool WriteDataById(int id);
+
+        bool WriteDataByAdresa(string adresa);
+
+        bool WriteDataByMesec(string mesec);
+    }
+}
